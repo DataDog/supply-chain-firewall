1c71ced2a9e60219de38fdb14a510c7cdb6d9efa
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-08-26 13:57:42 +0200

    merging with main

--- scfw/firewall.py
+++ scfw/firewall.py
@@ -85,32 +85,31 @@ def run_firewall() -> int:
     try:
 
         ddlog = logging.getLogger(LOG_DD)
-        cli_args, cli_command = parse_command_line()
-        # TODO: Print usage message and exit in this case
-        if not cli_command:
-            return 0
-        ecosystem, command = cli_command
 
-        pkgmgr_command = get_package_manager_command(ecosystem, command, executable=cli_args.executable)
-        if targets := pkgmgr_command.would_install():
+        args, help = parse_command_line()
+        if not args.command:
+            print(help)
+            return 0
+
+        command = get_package_manager_command(args.command, executable=args.executable)
+        if targets := command.would_install():
 
             verifiers = get_install_target_verifiers()
 
             if (findings := verify_install_targets(verifiers, targets)):
-                print_findings(findings)
                 tags = map(lambda x: f"{x.package}@{x.version}", findings.keys())
                 ddlog.info(f"Instalation was block while attempting to run {command}", extra={"tags":[tags]})
+                print_findings(findings)
                 print("\nThe installation request was blocked. No changes have been made.")
                 return 0
 
-            if cli_args.dry_run:
+            if args.dry_run:
                 print("Exiting without installing, no issues found for installation targets.")
                 return 0
 
-        # tags = map(lambda x: f"{x.package}@{x.version}", [InstallTarget("pip", "requests", "2.26.0")])
         tags = map(lambda x: f"{x.package}@{x.version}", targets)
-        ddlog.info(f"Running {command}", extra={"tags":tags})
-        pkgmgr_command.run()
+        ddlog.info(f"Running {args.command}", extra={"tags":tags})
+        command.run()
         return 0
 
     except Exception as e:

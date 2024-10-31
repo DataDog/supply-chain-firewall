7a9877a97e4c9044d5a8cbaad269951dbd9fbe8f
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2024-10-31 16:21:56 +0100

    Fix argparse bug

--- scfw/commands/npm_command.py
+++ scfw/commands/npm_command.py
@@ -167,7 +167,7 @@ def _npm_init_cli() -> ArgumentParser:
     parser = ArgumentParser(exit_on_error=False)
 
     # https://docs.npmjs.com/cli/v10/commands/npm-init
-    parser.add_argument("initializer", type=str, default=None, required=False)
+    parser.add_argument("initializer", type=str, default=None, nargs="?")
     parser.add_argument("--init-author-name", type=str, default=None)
     parser.add_argument("--init-author-url", type=str, default=None)
     parser.add_argument("--init-licence", type=str, default=None)

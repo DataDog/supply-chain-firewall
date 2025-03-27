DataDog/supply-chain-firewall

commit c9098bd5d534cee6efa448d80301a5f604e94aed

Use ints to define firewall actions instead of strings

scfw/logger.py
@@ -15,9 +15,9 @@ class FirewallAction(Enum):
     The various actions the firewall may take in response to inspecting a
     package manager command.
     """
-    ALLOW = "ALLOW"
-    ABORT = "ABORT"
-    BLOCK = "BLOCK"
+    ALLOW = 0
+    ABORT = 1
+    BLOCK = 2
 
     def __lt__(self, other) -> bool:
         """
@@ -38,7 +38,7 @@ class FirewallAction(Enum):
                 f"'<' not supported between instances of '{self.__class__}' and '{other.__class__}'"
             )
 
-        return (self.name, other.name) in {("ALLOW", "ABORT"), ("ALLOW", "BLOCK"), ("ABORT", "BLOCK")}
+        return self.value < other.value
 
     def __str__(self) -> str:
         """
@@ -47,7 +47,7 @@ class FirewallAction(Enum):
         Returns:
             A `str` representing the given `FirewallAction` suitable for printing.
         """
-        return self.value
+        return self.name
 
 
 class FirewallLogger(metaclass=ABCMeta):

commit 5217df56f20a75cabb4593275cdf882773e2ec09

Fix log level parsing bug introduced in c9098bd

scfw/logger.py
@@ -5,6 +5,7 @@ completed run of the supply-chain firewall.
 
 from abc import (ABCMeta, abstractmethod)
 from enum import Enum
+from typing_extensions import Self
 
 from scfw.ecosystem import ECOSYSTEM
 from scfw.target import InstallTarget
@@ -48,6 +49,25 @@ class FirewallAction(Enum):
             A `str` representing the given `FirewallAction` suitable for printing.
         """
         return self.name
+
+    @classmethod
+    def from_string(cls, s: str) -> Self:
+        """
+        Convert a string into a `FirewallAction`.
+
+        Args:
+            s: The `str` to be converted.
+
+        Returns:
+            The `FirewallAction` referred to by the given string.
+
+        Raises:
+            ValueError: The given string does not refer to a valid `FirewallAction`.
+        """
+        mappings = {f"{action}".lower(): action for action in cls}
+        if (action := mappings.get(s.lower())):
+            return action
+        raise ValueError(f"Invalid firewall action '{s}'")
 
 
 class FirewallLogger(metaclass=ABCMeta):

scfw/loggers/dd_logger.py
@@ -66,12 +66,13 @@ class DDLogger(FirewallLogger):
             logger: A configured log handle to which logs will be written.
         """
         self._logger = logger
+        self._level = _DD_LOG_LEVEL_DEFAULT
 
         try:
-            self._level = FirewallAction(os.getenv(DD_LOG_LEVEL_VAR))
+            if (dd_log_level := os.getenv(DD_LOG_LEVEL_VAR)) is not None:
+                self._level = FirewallAction.from_string(dd_log_level)
         except ValueError:
             _log.warning(f"Undefined or invalid Datadog log level: using default level {_DD_LOG_LEVEL_DEFAULT}")
-            self._level = _DD_LOG_LEVEL_DEFAULT
 
     def log(
         self,
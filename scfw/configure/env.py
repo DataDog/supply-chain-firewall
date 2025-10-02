b5c86661a6e4d2388c517ca4cc5d3f0c9be20a81
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2025-10-02 18:41:06 +0200

    Make a backup copy in case restoration fails

--- scfw/configure/env.py
+++ scfw/configure/env.py
@@ -2,10 +2,15 @@
 Provides utilities for configuring the environment (via `.rc` files) for using Supply-Chain Firewall.
 """
 
+import logging
 from pathlib import Path
 import re
+import os
+import tempfile
 
 from scfw.configure.constants import DD_AGENT_PORT_VAR, DD_API_KEY_VAR, DD_LOG_LEVEL_VAR, SCFW_HOME_VAR
+
+_log = logging.getLogger(__name__)
 
 _CONFIG_FILES = [".bashrc", ".zshrc"]
 
@@ -36,21 +41,44 @@ def _update_config_file(config_file: Path, answers: dict):
     def enclose(config: str) -> str:
         return f"{_BLOCK_START}{config}\n{_BLOCK_END}"
 
+    def overwrite_file(f, contents: str):
+        f.truncate(0)
+        f.write(contents)
+
     config = _format_answers(answers)
 
     pattern = f"{_BLOCK_START}(.*?){_BLOCK_END}"
     if not config:
         pattern = f"\n{pattern}\n"
 
+    # Make a backup copy of the original file in case something goes wrong
+    # during the update and we are unable to restore the original ourselves
+    temp_fd, temp_file = tempfile.mkstemp(text=True)
+
     with open(config_file, "r+") as f:
         original = f.read()
+
+        temp_handle = os.fdopen(temp_fd, 'w')
+        temp_handle.write(original)
+        temp_handle.close()
 
         updated = re.sub(pattern, enclose(config) if config else '', original, flags=re.DOTALL)
         if updated == original and config not in original:
             updated = f"{original}\n{enclose(config)}\n"
 
-        f.truncate(0)
-        f.write(updated)
+        try:
+            overwrite_file(f, updated)
+        except Exception as e:
+            _log.warning(f"Failed to add configuration to {config_file}: {e}")
+            try:
+                overwrite_file(f, original)
+            except Exception:
+                raise RuntimeError(
+                    f"Failed while restoring {config_file} after failed configuration update: "
+                    f"a backup copy of the original file contents is in {temp_file}"
+                )
+
+    os.remove(temp_file)
 
 
 def _format_answers(answers: dict) -> str:

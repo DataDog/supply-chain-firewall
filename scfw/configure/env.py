5b2c0e137c0ace6f71d596d455c01e99c0141c33
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2025-10-03 09:54:27 +0200

    Revert refactor to allow adding tests

--- scfw/configure/env.py
+++ scfw/configure/env.py
@@ -24,25 +24,36 @@ def update_config_files(answers: dict):
         answers: A `dict` configuration options to format and write to each file.
     """
     scfw_config = _format_answers(answers)
-    scfw_block = f"{_BLOCK_START}{scfw_config}\n{_BLOCK_END}" if scfw_config else ""
 
     for config_file in [Path.home() / file for file in _CONFIG_FILES]:
         if not config_file.exists():
             _log.info(f"Skipped adding configuration to file {config_file}: file does not already exist")
             continue
 
-        with open(config_file, "r+") as f:
-            original_config = f.read()
-
-            updated_config = re.sub(f"{_BLOCK_START}(.*?){_BLOCK_END}", scfw_block, original_config, flags=re.DOTALL)
-            if updated_config == original_config and scfw_config not in original_config:
-                updated_config = f"{original_config}\n{scfw_block}\n"
-
-            f.seek(0)
-            f.write(updated_config)
-            f.truncate()
-
+        _update_config_file(config_file, scfw_config)
         _log.info(f"Successfully added configuration to file {config_file}")
+
+
+def _update_config_file(config_file: Path, scfw_config: str):
+    """
+    Update the Supply-Chain Firewall configuration in the given file.
+
+    Args:
+        config_file: A `Path` to the configuration file to update.
+        scfw_config: A `str` containing the formatted configuration options to write.
+    """
+    scfw_block = f"{_BLOCK_START}\n{scfw_config}{_BLOCK_END}" if scfw_config else ""
+
+    with open(config_file, "r+") as f:
+        original_config = f.read()
+
+        updated_config = re.sub(f"{_BLOCK_START}(.*?){_BLOCK_END}", scfw_block, original_config, flags=re.DOTALL)
+        if updated_config == original_config and scfw_config not in original_config:
+            updated_config = f"{original_config}\n{scfw_block}\n"
+
+        f.seek(0)
+        f.write(updated_config)
+        f.truncate()
 
 
 def _format_answers(answers: dict) -> str:
@@ -58,18 +69,18 @@ def _format_answers(answers: dict) -> str:
     config = ''
 
     if answers.get("alias_npm"):
-        config += '\nalias npm="scfw run npm"'
+        config += 'alias npm="scfw run npm"\n'
     if answers.get("alias_pip"):
-        config += '\nalias pip="scfw run pip"'
+        config += 'alias pip="scfw run pip"\n'
     if answers.get("alias_poetry"):
-        config += '\nalias poetry="scfw run poetry"'
+        config += 'alias poetry="scfw run poetry"\n'
     if (dd_agent_port := answers.get("dd_agent_port")):
-        config += f'\nexport {DD_AGENT_PORT_VAR}="{dd_agent_port}"'
+        config += f'export {DD_AGENT_PORT_VAR}="{dd_agent_port}"\n'
     if (dd_api_key := answers.get("dd_api_key")):
-        config += f'\nexport {DD_API_KEY_VAR}="{dd_api_key}"'
+        config += f'export {DD_API_KEY_VAR}="{dd_api_key}"\n'
     if (dd_log_level := answers.get("dd_log_level")):
-        config += f'\nexport {DD_LOG_LEVEL_VAR}="{dd_log_level}"'
+        config += f'export {DD_LOG_LEVEL_VAR}="{dd_log_level}"\n'
     if (scfw_home := answers.get("scfw_home")):
-        config += f'\nexport {SCFW_HOME_VAR}="{scfw_home}"'
+        config += f'export {SCFW_HOME_VAR}="{scfw_home}"\n'
 
     return config

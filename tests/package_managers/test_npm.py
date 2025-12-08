c4c570f5b2e4e2ea84d2a0af7e9623a0b15557a1
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2025-12-08 09:38:55 +0100

    Account for erroring older npm versions in list command

--- tests/package_managers/test_npm.py
+++ tests/package_managers/test_npm.py
@@ -388,16 +388,27 @@ def test_options_prevent_install_new_npm_project(new_npm_project, command_line: 
     _backend_test_no_change(new_npm_project, command_line)
 
 
-def get_npm_project_state(path: Path, lockfile_only: bool = False) -> str:
+def get_npm_project_state(project_path: Path, lockfile_only: bool = False) -> str:
     """
-    Return the current state of installed packages in the npm project at `path`,
+    Return the current state of installed packages in the given npm project,
     optionally only considering what is contained in the `package-lock.json` file.
     """
+    check = True
     npm_list_command = ["npm", "list", "--all"]
+
+    lockfile_path = project_path / "package-lock.json"
+    node_modules_path = project_path / "node_modules/"
+
     if lockfile_only:
         npm_list_command += ["--package-lock-only"]
+    if lockfile_path.is_file() and not node_modules_path.is_dir():
+        # On older versions of npm, this condition results in an error code
+        # being returned, even though the command returns useful output
+        check = False
 
-    return subprocess.run(npm_list_command, check=True, text=True, capture_output=True).stdout
+    npm_list = subprocess.run(npm_list_command, check=check, text=True, capture_output=True, cwd=project_path)
+
+    return npm_list.stdout.strip()
 
 
 def _backend_test_npm_install_package_lock_only(project: Path, command_line: list[str]):

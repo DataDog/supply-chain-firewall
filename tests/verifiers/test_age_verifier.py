b27b87b4d863510d529f20027fa73c06ba4e2b53
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2026-01-07 16:45:30 +0100

    Rename PackageRecencyVerifier -> PackageAgeVerifier

--- tests/verifiers/test_age_verifier.py
+++ tests/verifiers/test_age_verifier.py
@@ -1,5 +1,5 @@
 """
-Tests of `PackageRecencyVerifier`.
+Tests of `PackageAgeVerifier`.
 """
 
 from datetime import timedelta
@@ -9,7 +9,7 @@ import pytest
 from scfw.ecosystem import ECOSYSTEM
 from scfw.package import Package
 from scfw.verifier import FindingSeverity
-from scfw.verifiers.recency_verifier import PackageRecencyVerifier
+from scfw.verifiers.age_verifier import PackageAgeVerifier
 
 # Version numbers are irrelevant for this verifier, so we use dummy values
 TEST_PACKAGES = [
@@ -21,12 +21,12 @@ TEST_PACKAGES = [
 @pytest.mark.parametrize("test_package", TEST_PACKAGES)
 def test_warn_on_recent_package(test_package: Package):
     """
-    Test that `PackageRecencyVerifier` has findings for a package that was created
-    outside of its configured recency tolerance.
+    Test that `PackageAgeVerifier` has findings for a package that does not have
+    the required minimum age.
     """
     # Verifier with tolerance of 1M years (should work for awhile)
-    recency_verifier = PackageRecencyVerifier()
-    recency_verifier.tolerance = timedelta(days=365*1000000)
+    recency_verifier = PackageAgeVerifier()
+    recency_verifier.minimum_age = timedelta(days=365*1000000)
 
     results = recency_verifier.verify(test_package)
     assert len(results) == 1
@@ -37,9 +37,9 @@ def test_warn_on_recent_package(test_package: Package):
 @pytest.mark.parametrize("test_package", TEST_PACKAGES)
 def test_no_warn_on_non_recent_package(test_package: Package):
     """
-    Test that `PackageRecencyVerifier` does not have findings for a package that was
-    created within its configured recency tolerance.
+    Test that `PackageAgeVerifier` does not have findings for a package that has
+    the required minimum age.
     """
-    recency_verifier = PackageRecencyVerifier()
+    recency_verifier = PackageAgeVerifier()
 
     assert not recency_verifier.verify(test_package)

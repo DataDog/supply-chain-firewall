69b8b391fe9ae974e5ac325209a82ab4156c85af
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2026-03-13 13:07:20 +0100

    Add package source specifies to Package

--- scfw/package.py
+++ scfw/package.py
@@ -3,8 +3,32 @@ A representation of software packages in supported ecosystems.
 """
 
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
 
 from scfw.ecosystem import ECOSYSTEM
+
+
+@dataclass(eq=True, frozen=True)
+class LocalPackageSource:
+    """
+    Specifies a local source for a software package.
+
+    Attributes:
+        local_source: A `Path` to the local source for the package.
+    """
+    local_source: Path
+
+
+@dataclass(eq=True, frozen=True)
+class RemotePackageSource:
+    """
+    Specifies a remote source for a software package.
+
+    Attributes:
+        remote_source: A `str` specifying the remote source for the package (e.g., download URL).
+    """
+    remote_source: str
 
 
 @dataclass(eq=True, frozen=True)
@@ -16,10 +40,12 @@ class Package:
         ecosystem: The package's ecosystem.
         name: The package's name.
         version: The package's version string.
+        source: Optional data specifying the package source (local or remote).
     """
     ecosystem: ECOSYSTEM
     name: str
     version: str
+    source: Optional[LocalPackageSource | RemotePackageSource] = None
 
     def __str__(self) -> str:
         """
@@ -36,3 +62,25 @@ class Package:
                 return f"{self.name}@{self.version}"
             case ECOSYSTEM.PyPI:
                 return f"{self.name}-{self.version}"
+
+    def get_local_source(self) -> Optional[LocalPackageSource]:
+        """
+        Return the package's `source` if it is local.
+
+        Returns:
+            The package's `LocalPackageSource` attribute or `None` if the source
+            is undefined or remote.
+        """
+        if isinstance(self.source, LocalPackageSource):
+            return self.source
+
+    def get_remote_source(self) -> Optional[RemotePackageSource]:
+        """
+        Return the package's `source` if it is remote.
+
+        Returns:
+            The package's `RemotePackageSource` attribute or `None` if the source
+            is undefined or remote.
+        """
+        if isinstance(self.source, RemotePackageSource):
+            return self.source

aab98fb8b6ea9e5884b1bea3fffa14a328f19cd4
Hannes Lohmander <hannes@lohmander.org>
2026-04-03 20:17:04 +0200

    chore: revert formatting

--- scfw/cli/__init__.py
+++ scfw/cli/__init__.py
@@ -33,7 +33,7 @@ def _add_audit_cli(parser: ArgumentParser):
         "package_manager",
         type=str,
         choices=SUPPORTED_PACKAGE_MANAGERS,
-        help="The package manager whose installed packages should be verified",
+        help="The package manager whose installed packages should be verified"
     )
 
     parser.add_argument(
@@ -41,7 +41,7 @@ def _add_audit_cli(parser: ArgumentParser):
         type=str,
         default=None,
         metavar="PATH",
-        help="Package manager executable to use for running commands (default: environmentally determined)",
+        help="Package manager executable to use for running commands (default: environmentally determined)"
     )
 
 
@@ -56,25 +56,25 @@ def _add_configure_cli(parser: ArgumentParser):
         "-r",
         "--remove",
         action="store_true",
-        help="Remove all Supply-Chain Firewall-managed configuration",
+        help="Remove all Supply-Chain Firewall-managed configuration"
     )
 
     parser.add_argument(
         "--alias-npm",
         action="store_true",
-        help="Add shell aliases to always run npm commands through Supply-Chain Firewall",
+        help="Add shell aliases to always run npm commands through Supply-Chain Firewall"
     )
 
     parser.add_argument(
         "--alias-pip",
         action="store_true",
-        help="Add shell aliases to always run pip commands through Supply-Chain Firewall",
+        help="Add shell aliases to always run pip commands through Supply-Chain Firewall"
     )
 
     parser.add_argument(
         "--alias-poetry",
         action="store_true",
-        help="Add shell aliases to always run Poetry commands through Supply-Chain Firewall",
+        help="Add shell aliases to always run Poetry commands through Supply-Chain Firewall"
     )
 
     parser.add_argument(
@@ -82,7 +82,7 @@ def _add_configure_cli(parser: ArgumentParser):
         type=str,
         default=None,
         metavar="PORT",
-        help="Configure log forwarding to the local Datadog Agent on the given port",
+        help="Configure log forwarding to the local Datadog Agent on the given port"
     )
 
     parser.add_argument(
@@ -90,7 +90,7 @@ def _add_configure_cli(parser: ArgumentParser):
         type=str,
         default=None,
         metavar="KEY",
-        help="API key to use when forwarding logs via the Datadog API",
+        help="API key to use when forwarding logs via the Datadog API"
     )
 
     parser.add_argument(
@@ -99,7 +99,7 @@ def _add_configure_cli(parser: ArgumentParser):
         default=None,
         choices=[str(action) for action in FirewallAction],
         metavar="LEVEL",
-        help="Desired logging level for Datadog log forwarding (options: %(choices)s)",
+        help="Desired logging level for Datadog log forwarding (options: %(choices)s)"
     )
 
     parser.add_argument(
@@ -107,7 +107,7 @@ def _add_configure_cli(parser: ArgumentParser):
         type=str,
         default=None,
         metavar="PATH",
-        help="Directory that Supply-Chain Firewall can use as a local cache",
+        help="Directory that Supply-Chain Firewall can use as a local cache"
     )
 
 
@@ -128,7 +128,7 @@ def _add_run_cli(parser: ArgumentParser):
     parser.add_argument(
         "--dry-run",
         action="store_true",
-        help="Verify any installation targets but do not run the package manager command",
+        help="Verify any installation targets but do not run the package manager command"
     )
 
     group = parser.add_mutually_exclusive_group()
@@ -136,25 +136,25 @@ def _add_run_cli(parser: ArgumentParser):
     group.add_argument(
         "--allow-on-warning",
         action="store_true",
-        help="Non-interactively allow commands with only warning-level findings",
+        help="Non-interactively allow commands with only warning-level findings"
     )
 
     parser.add_argument(
         "--allow-unsupported",
         action="store_true",
-        help="Disable verification and allow commands for unsupported package manager versions",
+        help="Disable verification and allow commands for unsupported package manager versions"
     )
 
     group.add_argument(
         "--block-on-warning",
         action="store_true",
-        help="Non-interactively block commands with only warning-level findings",
+        help="Non-interactively block commands with only warning-level findings"
     )
 
     parser.add_argument(
         "--error-on-block",
         action="store_true",
-        help="Treat blocked commands as errors (useful for scripting)",
+        help="Treat blocked commands as errors (useful for scripting)"
     )
 
     parser.add_argument(
@@ -162,7 +162,7 @@ def _add_run_cli(parser: ArgumentParser):
         type=str,
         default=None,
         metavar="PATH",
-        help="Package manager executable to use for running commands (default: environmentally determined)",
+        help="Package manager executable to use for running commands (default: environmentally determined)"
     )
 
 
@@ -196,18 +196,18 @@ class Subcommand(Enum):
             case Subcommand.Audit:
                 return {
                     "exit_on_error": False,
-                    "description": "Audit installed packages using Supply-Chain Firewall's verifiers.",
+                    "description": "Audit installed packages using Supply-Chain Firewall's verifiers."
                 }
             case Subcommand.Configure:
                 return {
                     "exit_on_error": False,
-                    "description": "Configure the environment for using Supply-Chain Firewall.",
+                    "description": "Configure the environment for using Supply-Chain Firewall."
                 }
             case Subcommand.Run:
                 return {
                     "usage": "%(prog)s [options] COMMAND",
                     "exit_on_error": False,
-                    "description": "Run a package manager command through Supply-Chain Firewall.",
+                    "description": "Run a package manager command through Supply-Chain Firewall."
                 }
 
     def _cli_spec(self) -> Callable[[ArgumentParser], None]:
@@ -253,7 +253,7 @@ def _cli() -> ArgumentParser:
         choices=_LOG_LEVELS,
         default=_DEFAULT_LOG_LEVEL,
         metavar="LEVEL",
-        help="Desired logging level (default: %(default)s, options: %(choices)s)",
+        help="Desired logging level (default: %(default)s, options: %(choices)s)"
     )
 
     subparsers = parser.add_subparsers(dest="subcommand", required=True)
@@ -304,21 +304,17 @@ def _parse_command_line(argv: list[str]) -> tuple[Optional[Namespace], str]:
         if (
             args.subcommand == Subcommand.Configure
             and args.remove
-            and any(
-                {
-                    args.alias_npm,
-                    args.alias_pip,
-                    args.alias_poetry,
-                    args.dd_agent_port,
-                    args.dd_api_key,
-                    args.dd_log_level,
-                    args.scfw_home,
-                }
-            )
+            and any({
+                args.alias_npm,
+                args.alias_pip,
+                args.alias_poetry,
+                args.dd_agent_port,
+                args.dd_api_key,
+                args.dd_log_level,
+                args.scfw_home,
+            })
         ):
-            raise ArgumentError(
-                None, "Cannot combine configuration and removal options"
-            )
+            raise ArgumentError(None, "Cannot combine configuration and removal options")
 
         return args, help_msg
 

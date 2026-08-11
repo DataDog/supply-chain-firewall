516513cfb1e7f6559b10af52d29fce012c6b86ea
Marc Wieser <marc.wieser@datadoghq.com>
2026-08-11 16:57:33 +0200

    Remove legacy supply chain firewall CLI

--- scfw/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Supply Chain Firewall (SCFW) is a tool for preventing the installation of malicious npm and PyPI packages.
-"""
-
-__version__ = "3.1.0"
--- scfw/package_managers/npm/common.py
+++
@@ -1,9 +0,0 @@
-"""
-Provides common definitions related to npm.
-"""
-
-# URI scheme for npm local package dependencies
-FILE_URI_PREFIX = "file:"
-
-# Prefix for all installed-package entries in package-lock.json
-NODE_MODULES_PREFIX = "node_modules/"
